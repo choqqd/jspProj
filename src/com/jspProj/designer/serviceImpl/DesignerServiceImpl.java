40ce2f5303f418a187e3932828be0f475aa39b4c
admin <admin@DESKTOP-2NVHVIH>
2021-05-28 15:37:08 +0900

    3:40

--- src/com/jspProj/designer/serviceImpl/DesignerServiceImpl.java
+++ src/com/jspProj/designer/serviceImpl/DesignerServiceImpl.java
@@ -1,5 +1,9 @@
 package com.jspProj.designer.serviceImpl;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.jspProj.common.DAO;
@@ -7,11 +11,32 @@ import com.jspProj.designer.service.DesignerService;
 import com.jspProj.designer.vo.DesignerVO;
 
 public class DesignerServiceImpl extends DAO implements DesignerService{
-
+	PreparedStatement psmt;
+	ResultSet rs;
+	String sql ="";
+	
 	@Override
 	public List<DesignerVO> DesignerSelectList() {
-		// TODO Auto-generated method stub
-		return null;
+		sql = "select * from designer";
+		List<DesignerVO> list = new ArrayList<>();
+		
+		try {
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while(rs.next()) {
+				DesignerVO vo = new DesignerVO();
+				vo.setDsCode(rs.getInt("ds_code"));
+				vo.setDsImage(rs.getString("ds_image"));
+				vo.setDsLikeIt(rs.getInt("like_It"));
+				vo.setDsName(rs.getString("ds_name"));
+				list.add(vo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return list;
 	}
 
 	@Override
@@ -36,6 +61,31 @@ public class DesignerServiceImpl extends DAO implements DesignerService{
 	public int deleteDesigner(DesignerVO vo) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public void close() {
+
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (psmt != null) {
+			try {
+				psmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- src/com/jspProj/designer/web/DesignerPage.java
+++ src/com/jspProj/designer/web/DesignerPage.java
@@ -1,15 +1,24 @@
 package com.jspProj.designer.web;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.jspProj.common.DbCommand;
+import com.jspProj.designer.service.DesignerService;
+import com.jspProj.designer.serviceImpl.DesignerServiceImpl;
+import com.jspProj.designer.vo.DesignerVO;
 
 public class DesignerPage implements DbCommand {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-
+		DesignerService service = new DesignerServiceImpl();
+		List<DesignerVO> list = service.DesignerSelectList();
+		
+		request.setAttribute("list", list);
+		
 		return "desiner/desinerPage.tiles";
 	}
 
